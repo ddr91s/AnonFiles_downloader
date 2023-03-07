2dcc5d884228142b08157bbf94ddb0736c492127
ddr91s <115823154+ddr91s@users.noreply.github.com>
2023-03-07 21:23:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from bs4 import BeautifulSoup as bs
 import requests, os, sys
 soup = bs(requests.get(sys.argv[1]).text, "html.parser")
 url = soup.find('a',id='download-url')['href']
-os.system(f"aria2c -c -j1 -x16 -s16 -k1m -d /var/www/html --check-certificate=false {url}")
+os.system(f"aria2c -c -j1 -x16 -s16 -k1m -d ./javsubs91/download --check-certificate=false --summary-interval=30 {url}")
