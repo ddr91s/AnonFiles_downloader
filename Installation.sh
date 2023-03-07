07b8e1cda261336d2e0bb088d019580c0b64ec73
ddr91s <115823154+ddr91s@users.noreply.github.com>
2023-03-07 21:39:46 +0700

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -4,8 +4,7 @@ apt install wget curl ca-certificates &> /dev/null
 wget -N git.io/aria2.sh &> /dev/null && chmod +x aria2.sh &> /dev/null
 echo 1|./aria2.sh &> /dev/null
 echo 12|./aria2.sh &> /dev/null
+rm -r ./aria2.sh
 cd ./javsubs91
 wget https://raw.githubusercontent.com/ddr91s/AnonFiles_downloader/main/main.py &> /dev/null && wget https://raw.githubusercontent.com/ddr91s/AnonFiles_downloader/main/lib.txt &> /dev/null
 python -m pip install -r ./lib.txt &> /dev/null
-cd ./
-rm -r ./aria2.sh
